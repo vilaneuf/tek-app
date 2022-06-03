b5c023fc7109805c120f9f2f878b2532cb423e4e
pvilanova <pvilanova@tekniker.es>
2022-06-03 13:25:53 +0200

    test push

---
+++ build/zephyr/nrf5340dk_nrf5340_cpuapp.dts.pre.d
@@ -0,0 +1,15 @@
+empty_file.o: C:/nrf_workspace/test1_5/zephyr/misc/empty_file.c \
+ C:/nrf_workspace/test1_5/zephyr/boards/arm/nrf5340dk_nrf5340/nrf5340dk_nrf5340_cpuapp.dts \
+ C:/nrf_workspace/test1_5/zephyr/dts/arm/nordic/nrf5340_cpuapp_qkaa.dtsi \
+ C:/nrf_workspace/test1_5/zephyr/dts/common/mem.h \
+ C:/nrf_workspace/test1_5/zephyr/dts/arm/nordic/nrf5340_cpuapp.dtsi \
+ C:/nrf_workspace/test1_5/zephyr/dts/arm/armv8-m.dtsi \
+ C:/nrf_workspace/test1_5/zephyr/dts/common/skeleton.dtsi \
+ C:/nrf_workspace/test1_5/zephyr/dts/arm/nordic/nrf_common.dtsi \
+ C:/nrf_workspace/test1_5/zephyr/include/dt-bindings/i2c/i2c.h \
+ C:/nrf_workspace/test1_5/zephyr/include/dt-bindings/gpio/gpio.h \
+ C:/nrf_workspace/test1_5/zephyr/dts/arm/nordic/override.dtsi \
+ C:/nrf_workspace/test1_5/zephyr/dts/arm/nordic/nrf5340_cpuapp_peripherals.dtsi \
+ C:/nrf_workspace/test1_5/zephyr/boards/arm/nrf5340dk_nrf5340/nrf5340_cpuapp_common.dts \
+ C:/nrf_workspace/test1_5/zephyr/boards/arm/nrf5340dk_nrf5340/nrf5340_cpuapp_partition_conf.dts \
+ C:/nrf_workspace/test1_5/zephyr/boards/arm/nrf5340dk_nrf5340/nrf5340_shared_sram_planning_conf.dts
